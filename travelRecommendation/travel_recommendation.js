119b8f800321cc40c2af21e272f85d28d4e4ef09
LukasLikesPython <Lukas.Huth@gmx.net>
2024-09-02 09:24:11 -0400

    project

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,6 +1,7 @@
 const btnSearch = document.getElementById("btnSearch");
 const btnClear = document.getElementById("btnClear");
 const searchBar = document.getElementById("searchBar");
+const recUl = document.getElementById("recommendations");
 
 btnSearch.addEventListener("click", handleSearchClick, true);
 btnClear.addEventListener("click", handleClearClick, true);
@@ -23,14 +24,14 @@ const travelData = fetch("./travel_recommendation_api.json")
 
 
 function handleClearClick(){
-    searchBar.value = "";
+    recUl.innerHTML = '';
 }
 
 
 async function handleSearchClick(){
     var data = await travelData;
     var searchStr = searchBar.value.toLowerCase()
-    var searchData;
+    var searchData = [];
 
     if (['beach', 'beaches'].includes(searchStr)){
         searchData = data['beaches'];
@@ -39,10 +40,20 @@ async function handleSearchClick(){
         searchData = data['temples'];
     }
     else if (['countries', 'country'].includes(searchStr)){
-        searchData = data['countries'];
+        console.log(data['countries'])
+        for(var i = 0; i < data['countries'].length; i++){
+            searchData = searchData.concat(data['countries'][i].cities)
+        }
     }
     else{
         alert('Keyword not found, try "beaches", "temples", or "countries".');
     }
-    console.log(searchData);
+    console.log(searchData)
+    handleClearClick();
+    searchData.forEach(destination => {
+        const markup = `<li><img src=${destination.imageUrl}>
+                        <br><b>${destination.name}</b><br>
+                        <p>${destination.description}</p></li>`
+        recUl.insertAdjacentHTML('beforeend', markup)
+                    });
 }
